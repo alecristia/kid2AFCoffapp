cd036e2f3e66ed7660d3dfa5192ef1b84f79390c
Alex Cristia <alecristia@gmail.com>
2016-03-14 16:15:35 +0100

    preparing phonegap version

---
+++ exp_objects/spriteData_grpA_prompt.js
@@ -0,0 +1,76 @@
+//Spécifications des stimuli pour le groupe A:
+// ballePF
+// voiturePC
+// manteauPA
+// bananePF
+// yeuxPC
+// compotePA
+// bouteillePF
+// lunettesPC
+// maisonPA
+// bateauPF
+// fleurPC
+// poissonPA
+
+var spriteData = {
+	balle: {
+		start: 0,
+		onset: 1.273,
+		length: 1.765
+	},
+	voiture: {
+		start: 1.765,
+		onset: 3.093,
+		length: 2.072
+	},
+	manteau: {
+		start: 3.825,
+		onset: 5.097,
+		length: 1.906
+	},
+	banane: {
+		start: 5.736,
+		onset: 6.991,
+		length: 1.959
+	},
+	yeux: {
+		start: 7.597,
+		onset: 8.927,
+		length: 1.834
+	},
+	compote: {
+		start: 9.407,
+		onset: 10.682,
+		length: 2.009
+	},
+	bouteille: {
+		start: 11.426,
+		onset: 12.663,
+		length: 1.823
+	},
+	lunettes: {
+		start: 13.244,
+		onset: 14.567,
+		length: 1.978
+	},
+	maison: {
+		start: 15.219,
+		onset: 16.522,
+		length: 1.796
+	},
+	bateau: {
+		start: 17.053,
+		onset: 18.334,
+		length: 1.808
+	},
+	fleur: {
+		start: 18.861,
+		onset: 20.162,
+		length: 1.919
+	},
+	poisson: {
+		start: 20.777,
+		onset: 20.033,
+		length: 2.055
+	}
+};	
---
+++ exp_objects/spriteData_grpA_reward.js
@@ -0,0 +1,36 @@
+//Spécifications des stimuli pour le groupe A:
+// ballePF
+// voiturePC
+// manteauPA
+// bananePF
+// yeuxPC
+// compotePA
+// bouteillePF
+// lunettesPC
+// maisonPA
+// bateauPF
+// fleurPC
+// poissonPA
+
+var spriteRewardData = {
+	balle: {
+		start: 1.273,
+		onset: 1.273,
+		length: 1.000
+	},
+	voiture: {
+		start: 3.093,
+		onset: 3.093,
+		length: 1.000
+	},
+	manteau: {
+		start: 5.097,
+		onset: 5.097,
+		length: 1.000
+	},
+	banane: {
+		start: 6.991,
+		onset: 6.991,
+		length: 1.000
+	}
+};	
